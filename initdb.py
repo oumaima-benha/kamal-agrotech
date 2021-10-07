61d3b21261fd4a015363f7780856ba8a0858762b
oumaima benhallouk <o.benhallouk@gmail.com>
2021-10-07 12:54:36 +0100

    Importing data from Excel file into the database

--- initdb.py
+++ initdb.py
@@ -1,11 +1,15 @@
 import sqlite3
+import pandas as pd
+products = pd.read_excel(
+    'resources/db/test.xlsx', 
+    sheet_name='Sheet1',
+    header=0)
 connection = sqlite3.connect("resources/db/products.db")
 cursor = connection.cursor()
 cursor.execute("""
     CREATE TABLE products
     (Name TEXT, Category TEXT, Homologation TEXT, Composition TEXT, PackagingQuantity TEXT, PurchasePrice INTEGER, SellingPrice INTEGER, Quantity INTEGER)
 """)
-cursor.execute("""INSERT INTO products VALUES 
-    ('test', 'Herbicide', 'E02-7-009', 'elementx 10%', '1L', 22, 40, 100)
-""")
+products.to_sql('products', connection, if_exists='append', index=False)
+
 connection.commit()
